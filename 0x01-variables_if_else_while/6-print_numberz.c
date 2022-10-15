4db5868ab3c2b769bfa4026ec760d07e8e3455bd
fdblay <fdblay@gmail.com>
2022-10-14 20:31:45 -0700

    putchar_0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digits using the putchar function
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char int_n;
+
+	for (int_n = 48; int_n <= 57; int_n++)
+	{
+		putchar(int_n);
+	}
+
+	putchar(10);
+	return (0);
+}
