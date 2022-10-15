f8adf85f0995d109e399fa5a8d2c092a3a1cffc7
fdblay <fdblay@gmail.com>
2022-10-14 19:53:35 -0700

    single_digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all single base 10 numbers from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 A code that prints a random number to (n)
 The alphabet game
 A program that prints the alphabet in lowercase, and then in uppercase.
+Program prints all single numbers of base 10 starting from 0
