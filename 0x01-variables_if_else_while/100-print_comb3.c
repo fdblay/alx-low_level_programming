a64954223fd8848faa1bfeea189ba4d0f7594641
fdblay <fdblay@gmail.com>
2022-10-16 14:33:51 -0700

    print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print numbers between 00 and 89
+ * Return: Always 0
+ */
+int main(void)
+{
+	int f, k;
+
+	f = 48;
+	k = 48;
+
+	while (f < 58)
+	{
+		k = 48;
+		while (k < 58)
+		{
+			if (k != f && k < f)
+			{
+			       putchar(f);
+		       	       putchar(k);
+			       if (k == 57 && f == 56)
+			       {
+				       break;
+			       }
+			       putchar(',');
+			       putchar(' ');
+			}
+			f++
+		}
+		k++
+	}
+	putchar(10);
+	return (0);
+}
