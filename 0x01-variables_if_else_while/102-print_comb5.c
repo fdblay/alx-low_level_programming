ece9363226a91b877ed027e41691e38c5daa2ad9
fdblay <fdblay@gmail.com>
2022-10-16 12:50:27 -0700

    numbers 00 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,35 +11,35 @@ int main(void)
 	while (d < 58)
 	{
 		e = 48;
-	while (e < 58)
-	{
-		r = 48;
-	while (r < 58)
-	{
-		f = 48;
-	while (f < 58)
-	{
-		n = (d + 10) + e;
-		m = (r + 10) + f;
-	if (n < m)
-	{
-		putchar(d);
-		putchar(e);
-		putchar(' ');
-		putchar(r);
-		putchar(f);
-	if (d == 57 && e == 56 && r == 57 && f == 57)
-		break;
-		putchar(',');
-		putchar(' ');
-	}
-	f++;
-	}
-	r++;
-	}
-	e++;
-	}
-	d++;
+		while (e < 58)
+		{
+			r = 48;
+			while (r < 58)
+			{
+				f = 48;
+				while (f < 58)
+				{
+					n = (d + 10) + e;
+					m = (r + 10) + f;
+					if (n < m)
+					{
+						putchar(d);
+						putchar(e);
+						putchar(' ');
+						putchar(r);
+						putchar(f);
+						if (d == 57 && e == 56 && r == 57 && f == 57)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					f++;
+				}
+				r++;
+			}
+			e++;
+		}
+		d++;
 	}
 	putchar(10);
 	return (0);
