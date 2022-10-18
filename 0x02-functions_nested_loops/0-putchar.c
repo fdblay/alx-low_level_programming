674ed9b23beab7cdfacc3ad8df5f430a9cbc9e94
fdblay <fdblay@gmail.com>
2022-10-18 14:29:48 -0700

    printing with _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _putchar - writes a character c to stdout
+* @c: Ther character to print
+*
+* Return: On success 1.
+*/
+int _putchar(char c)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+
+	return (1);
+}
