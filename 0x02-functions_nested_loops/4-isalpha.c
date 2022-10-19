16a4e09d084bcc8230f12433d891631aa7019bcc
fdblay <fdblay@gmail.com>
2022-10-18 22:45:12 -0700

    Function checks for both lower and upper case alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isalpha - checks for a lowercase or uppercase alphabet
+* @c: the character to check
+* Return: 1 if True and 0 if False
+*/
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 90 || c >= 97 && c <= 122);
+}
