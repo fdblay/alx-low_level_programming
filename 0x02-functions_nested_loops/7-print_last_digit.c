87cd6e47340e8d45afe380377b0b9889af5c701f
fdblay <fdblay@gmail.com>
2022-10-19 02:37:43 -0700

    print last digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_last_digit - prints the last digit of a number
+* @i: parameter
+* Return: last digit
+*/
+int print_last_digit(int i)
+{
+	if (i > 0)
+	{
+		i = i;
+	}
+	return (i % 10);
+}
