9cce757e4be5314dc2c87d27e13cd3a33fe40430
fdblay <fdblay@gmail.com>
2022-10-19 01:54:37 -0700

    absolute value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _abs - computes the absolute value of an integer
+* @i: parameter
+* Return: Absolute value of a number
+*/
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		i = -i;
+	}
+	return (i);
+}
