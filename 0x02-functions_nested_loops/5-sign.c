9a9bf64ce5e8e426ea6421fdff8ad9f3143c025f
fdblay <fdblay@gmail.com>
2022-10-19 00:43:00 -0700

    prints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,26 @@
 #include "main.h"
 /**
 * print_sign - prints teh sign of a number
-* @n: holds the integer value
+* @n: holds the value of the integer
 * Return: 1 & print + or 0 & print 0 and -1 & print - based on the condition
 */
 int print_sign(int n)
 {
-	return ((n > 0) || (n == 0) || (n < 0));
+	int n;
+
+	if (n > 48)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 48)
+	{
+		_putchar(45);
+		return (-1);
+	}
 }
