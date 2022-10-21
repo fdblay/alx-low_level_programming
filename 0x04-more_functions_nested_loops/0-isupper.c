b898a1b877087874e21d734408034015f39ff34f
fdblay <fdblay@gmail.com>
2022-10-20 17:36:25 -0700

    is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - checks for uppercase character
+* @c: formal parameter passed
+* Return:1 0r 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+
+	}
+	return (1);
+
+	else
+	{
+
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+isupper; a function that checks for uppercase character
