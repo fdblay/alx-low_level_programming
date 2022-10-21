7aa45efc5f674c4b51873ea86766eb90486e2fd8
fdblay <fdblay@gmail.com>
2022-10-21 10:38:03 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,13 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
 * _isupper - checks for uppercase character
 * @c: formal parameter passed
-* Return: 1 or 0
+* Return: on sucess 1 else 0
 */
 
 int _isupper(int c)
 {
-	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+	return ((c >= 65 && c <= 90) || (c != 65 && c != 90));
 }
