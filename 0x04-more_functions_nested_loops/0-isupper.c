917ee3c2f41bf391d8c1c35929186544c84eebd7
fdblay <fdblay@gmail.com>
2022-10-20 18:04:08 -0700

    is uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,20 +3,10 @@
 /**
 * _isupper - checks for uppercase character
 * @c: formal parameter passed
-* Return: 0
+* Return: 1 or 0
 */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		_putchar(49);
-
-		else
-		{
-		_putchar(48);
-		}
-
-	}
-	return (0);
+	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
 }
