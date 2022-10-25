5cb87dd05e964d98301f25f34ca6075e517fd0d0
fdblay <fdblay@gmail.com>
2022-10-25 09:54:43 -0700

    string reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,9 +7,12 @@
 */
 void print_rev(char *s)
 {
-	if (*s != '\0')
-	{
-		print_rev(s + 1);
-		_putchar(*s + 0);
-	}
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar(10);
 }
