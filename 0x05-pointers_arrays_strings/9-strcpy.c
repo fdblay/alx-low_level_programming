bd9589e1576d4c804e2189c5c9b09a8d40176a1c
fdblay <fdblay@gmail.com>
2022-10-25 15:31:53 -0700

    copy to

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _strcpy - copy char src to dest
+* @dest: first parameter
+* @src: second parameter
+* Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src);
+{
+	_strcpy(*dest, *src);
+	_putchar(*dest);
+
+	_putchar(10);
+	return (dest);
+}
