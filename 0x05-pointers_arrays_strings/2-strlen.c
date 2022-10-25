aeb1da5198e21b3b015c221a690c566da5eaa078
fdblay <fdblay@gmail.com>
2022-10-25 04:59:45 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s: parameter passed
+* Return: Always 0
+*/
+int _strlen(char *s)
+{
+	len = _strlen(*s);
+	printf("strlen(*s): %d\n", len);
+	return (0);
+
+}
