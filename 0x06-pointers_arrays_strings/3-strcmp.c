7be81f3a12f970de3c301f8b7a97b33494f7fe9c
fdblay <fdblay@gmail.com>
2022-10-27 00:37:59 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strcmp - compares two strings
+* @s1: first parameter
+* @s2: second parameter
+* Return: result;
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = _strcmp(s1, s2);
+	return (result);
+}
