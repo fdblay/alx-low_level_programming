2e64924288a1f5c849624d81ade69c2763f75307
fdblay <fdblay@gmail.com>
2022-11-03 02:54:40 -0700

    factorial (n!)

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,12 +3,14 @@
 /**
 * factorial - returns the factorial of a given number
 * @n: parameter passed
-* Return: factorial if true and -1 to indicate an error
+* Return: factorial if true, 1 when factorial is  0 and -1 to indicate an error
 */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+	else if (n == 0)
+		return (1);
 	else
 		return (n * factorial(n - 1));
 }
