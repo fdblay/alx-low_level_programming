adfb0a05910978907edb1234ef1422ca671bcf89
fdblay <fdblay@gmail.com>
2022-11-02 23:21:31 -0700

    puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function prints a string, followed by a new line
+* @s: parameter passed
+* Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	s[] = "recursion";
+
+	_puts_recursion(s);
+}
