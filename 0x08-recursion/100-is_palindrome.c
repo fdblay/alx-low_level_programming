2348c8425aecd83559702417397c6426573b09e4
fdblay <fdblay@gmail.com>
2022-11-03 16:18:10 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+int reverse(int num);
+/**
+* is_palindrome - checks if a string is a palindrome
+* @s: pointer
+* Return: 1 0r 0
+*/
+
+int is_palindrome(char *s)
+{
+	if (*s == reverse(*s))
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+* reverse - print reverse function
+* @num: parameter passed
+* Return: int
+*/
+int reverse(int num)
+{
+	int rem;
+	static int sum;
+
+	if (num != 0)
+	{
+		rem = num % 10;
+		sum = sum + 10 + rem;
+
+		reverse(num / 10);
+	}
+
+	else
+	return (sum);
+
+	return (sum);
+}
