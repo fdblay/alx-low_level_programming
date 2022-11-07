d031eeb7962e23e5dfd1c9ead5e590afb2c1709a
fdblay <fdblay@gmail.com>
2022-11-07 20:24:35 +0000

    0x07 pointers,array_strings

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - function name.
+ * @s: variable
+ * @to: variable
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
