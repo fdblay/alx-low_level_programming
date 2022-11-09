3c331ab4db058dcbf621ddbd3058137e23813030
fdblay <fdblay@gmail.com>
2022-11-09 16:55:41 +0000

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,21 +19,23 @@ char *str_concat(char *s1, char *s2)
 			++len1;
 	}
 	else
+	{
 		s1 = "";
+	}
 	if (s2)
 	{
 		while (s2[len2])
 			++len2;
 	}
 	else
+	{
 		s2 = "";
+	}
 
 	cat = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (!cat)
-	{
 		return (NULL);
-	}
 	if (s1)
 	{
 		for (len1 = 0; s1[len1]; ++len1)
@@ -42,7 +44,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2)
 	{
 		for (len2 = 0; s2[len2]; ++len2)
-			cat[len2] = s2[len2];
+			cat[len1 + len2] = s2[len2];
 	}
 	cat[len1 + len2] = '\0';
 	return (cat);
