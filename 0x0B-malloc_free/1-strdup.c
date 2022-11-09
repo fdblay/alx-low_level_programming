e6425eefdfc9e8cfd79a59e754b0b1b22be1b89f
fdblay <fdblay@gmail.com>
2022-11-09 13:01:47 +0000

    strup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a new memory space
+ * @str: char pointer passed
+ * Return: pointer or NULL
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+
+	str = _strdup(duplicate);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (duplicate);
+	}
+	free(str)
+	return (0);
+}
