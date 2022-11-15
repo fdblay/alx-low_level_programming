b596a4ec3372c63ad3e07cc0af36a05a4c9edaeb
fdblay <fdblay@gmail.com>
2022-11-15 07:36:03 +0000

    initialize struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog d = {"name", 2, "owner"};
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 
 	return (0);
 }
