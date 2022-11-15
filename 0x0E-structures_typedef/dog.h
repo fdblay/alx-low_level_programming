ea431390439cd766dfa8be605335f28c2ea16634
fdblay <fdblay@gmail.com>
2022-11-15 06:18:36 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,4 +20,21 @@ typedef struct dog
 	char *owner;
 } my_dog;
 
+/**
+ * struct dog - function name of type struct
+ * typedef - assigns a new alias to the funciton name
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ *
+ * intial function name was struct dog,
+ * typedef is used to assign dog_t as new name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
 #endif
