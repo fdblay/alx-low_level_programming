e64c3738e028bf310c98555042329bdaee35454f
fdblay <fdblay@gmail.com>
2022-11-16 03:03:00 +0000

    create a new struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,8 +64,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 
-	mydog = malloc(sizeof(dog_t));
-	if (dog_t == NULL)
+	mydog = malloc(dog_t);
+	if (mydog == NULL)
 		return (NULL);
 
 	mydog->name = malloc(sizeof(char) * (len1 + 1));
