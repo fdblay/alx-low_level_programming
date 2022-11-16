ec96f37593109a0949f667ec5c9ca5c52ef14138
fdblay <fdblay@gmail.com>
2022-11-16 10:47:02 +0000

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,15 @@
+#ifndef "0-object_like_macro.h"
+#define "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - defines a macro named SIZE
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+#define SIZE 1024
+}
+
+#endif /* 0-object_like_macro.h */
