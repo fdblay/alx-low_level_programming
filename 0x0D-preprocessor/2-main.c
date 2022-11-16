4ab22f8d39e0745271b5cc87f86f7099d10ab6d3
fdblay <fdblay@gmail.com>
2022-11-16 13:32:57 +0000

    print __FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,38 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "OBJECT_LIKE_MACRO.H"
 
 /**
- * _putchar - prints char to stdout
- * @c: parameter passed
- * Return: 1 or 0
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * main - prints out __FILE__
- * Return: 0
+ * main - prints the name of the file
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	char *ptr = __FILE__;
-	int len;
-
-	ptr = (char *) malloc(SIZE);
-	if (ptr == NULL)
-		return (NULL);
-	for (len = 0; len < ptr; len++)
-	{
-		if (*ptr[len] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		else
-			_putchar(*ptr[len]);
-	}
+	printf("%s\n", __FILE__);
 	return (0);
 }
