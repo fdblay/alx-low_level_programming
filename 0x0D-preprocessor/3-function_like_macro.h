92757ab25228eeef22a557a751687f39a79aa0e5
fdblay <fdblay@gmail.com>
2022-11-16 13:41:52 +0000

    macro ABS

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#include <stdio.h>
+
+/**
+ * ABS - computes the absolute value of x
+ * @x: value
+ */
+
+#define ABS(x) x
+
+#endif /* FUNCTION_LIKE_MACRO_H */
