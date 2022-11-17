bffe92967e00c6b045ab8f1381d8415e3112c107
fdblay <fdblay@gmail.com>
2022-11-17 11:24:01 +0000

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,3 +1,30 @@
 #include "3-calc.h"
 
 /**
+ * *get_op_func - function pointer
+ * @s: operator passed as arg to the program
+ *
+ * Return: pointer to function or NULL if s isn't a match
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,48 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program performs simple operations
+ * @argc: number of args
+ * @argv: array of args
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, calc;
+	char o;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && num2 == 0)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	calc = func(num1, num2);
+
+	printf("%d\n", calc);
+
+	return (0);
+}
