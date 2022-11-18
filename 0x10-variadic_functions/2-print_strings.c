8bbe8077c453c2e58091f419dce9a4b40f3565d1
fdblay <fdblay@gmail.com>
2022-11-18 19:45:45 +0000

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,8 +26,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 		{
 			str = "(nil)";
-			printf("%s", str);
+
 		}
+
+		printf("%s", str);
 
 		if (i != (n - 1))
 		{
