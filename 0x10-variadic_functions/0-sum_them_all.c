790ed424a5d6440405edfb8fdc1c059f6e723883
fdblay <fdblay@gmail.com>
2022-11-18 06:37:17 +0000

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,7 +6,7 @@
  * sum_them_all - returns the sum of all its parameters
  * @n: first argument passed
  *
- * Return: 0 or NULL if n == 0
+ * Return: 0 if n == 0
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -16,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 
 	if (n == 0)
-		return (NULL);
+		return (0);
 
 	va_start(arglist, n);
 
