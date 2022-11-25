17ce42e725acef4ce5e733f55b8b59833a76e188
fdblay <fdblay@gmail.com>
2022-11-25 23:23:33 +0000

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_list - prints the elements of list_t list
+ * @h: pointer to list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (i);
+}
