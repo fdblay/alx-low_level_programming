fd2096200dffb4e2bef15eba77f53b11e5ba59da
fdblay <fdblay@gmail.com>
2022-11-25 23:28:00 +0000

    print_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,4 +28,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif /* LISTS_H */
