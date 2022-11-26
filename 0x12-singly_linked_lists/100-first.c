6b73895b3e7aefe494a61a76b049b5b72a352818
fdblay <fdblay@gmail.com>
2022-11-26 23:15:17 +0000

    print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * before_main - function executes before the main function
+ *
+ * Description: function will print out some strings before main
+ * executes
+ * Return: Always 0
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	return (0);
+}
+
