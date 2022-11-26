7690bea877498bc4d341d3266db546e9ca7f97d4
fdblay <fdblay@gmail.com>
2022-11-26 16:49:45 +0000

    add node infront and at the end

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/****** LIBRARIES ******/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/*** struct lists ***/
+
+/****** STRUCT LIST_S *********/
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,7 +16,6 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -22,6 +23,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/********** FUNCTION PROTOTYPES **********/
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
