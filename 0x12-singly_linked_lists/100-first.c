831367996e5deab77fee3f20ee3e31ceac26ceab
fdblay <fdblay@gmail.com>
2022-11-26 23:34:13 +0000

    print before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include <stdio.h>
 /**
  * before_main - function executes before the main function
  *
@@ -6,7 +6,7 @@
  * executes
  * Return: Always 0
  */
-
+void before_main() __attribute__((constructor));
 void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
