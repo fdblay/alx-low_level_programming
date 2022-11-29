73701755c38df798af597f1f05713f76f37fda52
fdblay <fdblay@gmail.com>
2022-11-29 08:25:41 +0000

    print number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in the linked list
+ * @h: pointer to the first element in the link
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+	//	printf("-> %lu elements\n", num);
+		h = h->next;
+	}
+	return (num);
+}
