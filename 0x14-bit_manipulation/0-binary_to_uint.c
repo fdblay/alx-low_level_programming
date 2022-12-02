23100a686a6f9ccc37bfe3f039ee08b2073a6aa4
fdblay <fdblay@gmail.com>
2022-12-02 22:43:39 +0000

    convert binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,7 +11,7 @@ unsigned int binary_to_uint(const char *b)
 	int total = 0, dvalue = 1;
 	int slen = strlen(b);
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
 	for (int i = (slen - 1); i >= 0; i--)
 	{
